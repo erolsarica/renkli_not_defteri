6aabf309fef61277c754464b065216856d7ddece
Erol SARICA <61289802+erolsarica@users.noreply.github.com>
2022-12-14 13:21:25 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/erolsarica/todoapp_yellow/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.erolsarica.todoapp_yellow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todoapp_yellow/homepage.dart';
+
+void main() async{
+
+  // Hive başlat
+  await Hive.initFlutter();
+
+  // open a box
+  var box = await Hive.openBox('mybox');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+          textTheme: TextTheme(bodyMedium: GoogleFonts.zillaSlab(fontWeight: FontWeight.bold, fontSize: 18)),
+          primarySwatch: Colors.yellow),
+
+    );
+  }
+}
