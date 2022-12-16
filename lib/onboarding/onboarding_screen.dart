b8682fdb47dde6504d7ccf2bf0efaf365c4f9c40
Erol SARICA <erol.sarica@boun.edu.tr>
2022-12-16 15:31:51 +0300

    onboarding screens updated

--- lib/onboarding/onboarding_screen.dart
+++ lib/onboarding/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:todoapp_yellow/homepage.dart';
 import 'package:todoapp_yellow/onboarding/intro_page_1.dart';
 import 'package:todoapp_yellow/onboarding/intro_page_2.dart';
 import 'package:todoapp_yellow/onboarding/intro_page_3.dart';
@@ -16,6 +17,10 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
   // controller to keep track of which page we're on
   PageController _controller = PageController();
 
+  // keep track of if we ate on the last page or not
+  bool onLastPage = false;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +28,11 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
         children: [
             PageView(
               controller: _controller,
+              onPageChanged: (index) {
+                setState(() {
+                  onLastPage = (index == 2);
+                });
+              },
               children: [
                 IntroPage1(),
                 IntroPage2(),
@@ -32,8 +42,44 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
 
           // dot indicators
           Container(
-              alignment: Alignment(0, 0.75),
-              child: SmoothPageIndicator(controller: _controller, count: 3))
+              alignment: const Alignment(0, 0.75),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+
+                  // skip
+                  GestureDetector(
+                    onTap: () {
+                      _controller.jumpToPage(2);
+                      },
+                      child: Text('Atla')
+                  ),
+
+                  // dot indicator
+                  SmoothPageIndicator(controller: _controller, count: 3),
+
+                  // next or done
+                  onLastPage
+                      ? GestureDetector(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                              return HomePage();
+                            },
+                            ),
+                        );
+                      },
+                      child: Text('Bitir'))
+                      : GestureDetector(
+                      onTap: () {
+                        _controller.nextPage(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeIn,
+                        );
+                      },
+                      child: Text('Sonraki')),
+                ],
+              ))
         ],
       ),
     );
